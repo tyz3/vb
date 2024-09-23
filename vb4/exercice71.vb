5a785d68e733b3693b713537adcf1ab1008018e2
thibaut <turgis.thibaut@gmail.com>
2024-09-23 16:26:56 +0200

    Update exercice71.vb

--- vb4/exercice71.vb
+++ vb4/exercice71.vb
@@ -1,7 +1,28 @@
-﻿Module exercice71
+﻿Module exercice71G1
 
     Sub Main()
+    Dim montant, remise, montantNet As Double
 
-    End Sub
+    Console.WriteLine("Veuillez taper votre montant")
+    montant = Console.ReadLine()
+    remise = 0
+    If montant > 5000 Then
+        remise = 2 / 100
+        Console.WriteLine("Remise de 2%")
+    ElseIf montant >= 2000 Then ' montant compris entre 2000 et 5000
+        remise = 1 / 100
+        Console.WriteLine("Remise de 1%")
+    Else
+        Console.WriteLine("Pas de remise")
+    End If
+
+    montantNet = montant * (1 - remise)
+
+    Console.WriteLine("Le montant net est : " + montantNet.ToString())
+    Console.WriteLine("Au revoir")
+
+    Console.ReadLine()
+
+End Sub
 
 End Module
