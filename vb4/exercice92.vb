4c072b6496f96f27d86e68eb01c277360728632a
thibaut <turgis.thibaut@gmail.com>
2024-09-30 16:44:55 +0200

    Add files via upload

---
+++ vb4/exercice92.vb
@@ -0,0 +1,24 @@
+﻿﻿Module exercice91
+    Sub Main()
+    Dim note, somme, moyenne, pourcentage As Double
+    Dim nbNotes, nbNotesSup10 As Integer
+
+    Console.WriteLine("Entrez une note (-1 pour fin) :")
+    note = Console.ReadLine()
+    While note <> -1
+        If note > 10 Then
+            nbNotesSup10 = nbNotesSup10 + 1
+        End If
+        somme = somme + note
+        nbNotes = nbNotes + 1
+        Console.WriteLine("Entrez une note (-1 pour fin) :")
+        note = Console.ReadLine()
+    End While
+    moyenne = somme / nbNotes
+    pourcentage = nbNotesSup10 / nbNotes * 100
+    Console.WriteLine("Vous avez " + pourcentage.ToString() + " % de notes > à 10")
+    Console.WriteLine("Votre moyenne est de  " + moyenne.ToString())
+    Console.ReadLine()
+End Sub
+
+End Module
